6d00de0e3f93e97407ed75975c6da2d63daba5ca
FreeFallRush <queenofcups.altbot@gmail.com>
2023-07-12 20:30:19 +0300

    imported handlers in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./style.css";
+import handlers from "./js/handlers";
+
+handlers.openExtraInfo();
+handlers.closeExtraInfo();
+handlers.openForecastInfo();
+handlers.closeForecastInfo();
+handlers.openGifSection();
+handlers.closeGifSection();
