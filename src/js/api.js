e27a464292b27091af6398421506c36de246c308
FreeFallRush <queenofcups.altbot@gmail.com>
2023-07-12 20:37:47 +0300

    added api.js to use weather api

---
+++ src/js/api.js
@@ -0,0 +1,19 @@
+const apiData = (() => {
+  const form = document.querySelector("form");
+  const loader = document.querySelector(".loader");
+  const switchUnitBtn = document.querySelector(".switch-checkbox");
+  const WAKey = "8095df91194a4c03b96164937232306";
+  const GAKey = "ok9Un0m6rvRm9ifbic2wG6Mb2ZlNMmg3";
+
+  const showWeather = async (locationInput) => {
+    let url = `https://api.weatherapi.com/v1/forecast.json?key=${WAKey}&q=${locationInput}&days=2`;
+    const response = await fetch(url, { mode: "cors" });
+    if (!response.ok) throw new Error(response.statusText);
+    const weatherData = await response.json();
+
+    console.log(weatherData);
+  };
+  return { showWeather };
+})();
+
+export default apiData;
